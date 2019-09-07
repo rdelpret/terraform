251c6c936fb045586a1c4bd5ebe817cbf1e9cd14
Robbie <darkcityagent@Roberts-MBP-3.fios-router.home>
2019-09-07 19:38:34 -0400

    added security group and instance

--- main.tf
+++ main.tf
@@ -3,10 +3,33 @@ provider "aws" {
 }
 
 resource "aws_instance" "webserver" {
-  ami           = "ami-0f2b4fc905b0bd1f1"
-  instance_type = "t2.micro"
-
+  ami                    = "ami-0c55b159cbfafe1f0"
+  instance_type	         = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow-http.id]
+	
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     Name = "DarkCityWeb01"
   }
 }
+
+resource "aws_security_group" "allow-http" {
+	name = "darkcityweb-sg"
+
+	ingress {
+		from_port   = 8080
+		to_port     = 8080
+		protocol    = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+		Name = "darkcityweb-sg"
+	}
+
+}
+	
