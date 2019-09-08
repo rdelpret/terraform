b3ea5f6b5e2a62f3bd01977af1ed8941db61049a
Robbie <darkcityagent@Roberts-MBP-3.fios-router.home>
2019-09-07 20:57:34 -0400

    added an ALB attached to an ASG

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ data "aws_subnet_ids" "default" {
 # SG
 
 resource "aws_security_group" "allow-http" {
-	name = "darkcityweb-sg"
+	name = "darkcitywebsever"
 
 	ingress {
 		from_port   = var.server_port
@@ -94,6 +94,9 @@ resource "aws_launch_configuration" "webcluster-lc" {
 resource "aws_autoscaling_group" "webcluster-asg" {
 	launch_configuration = aws_launch_configuration.webcluster-lc.name
 	vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+	target_group_arns    = [aws_lb_target_group.web-cluster-asg.arn]
+	health_check_type    = "ELB"
+	
 	min_size = 2
 	max_size = 10
 
@@ -108,6 +111,80 @@ resource "aws_autoscaling_group" "webcluster-asg" {
 	}
 }
 
+# ALB
+
+resource "aws_lb" "darkcityagent-dot-com" {
+	name               = "darkcityagent-dot-com"
+	load_balancer_type = "application"
+	subnets            = data.aws_subnet_ids.default.ids
+	security_groups    = [aws_security_group.http-alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+	load_balancer_arn = aws_lb.darkcityagent-dot-com.arn
+	port							= 80
+	protocol          = "HTTP"
+  
+	default_action {
+		type = "fixed-response"
+
+		fixed_response {
+			content_type = "text/plain"
+			message_body = "404: Page not found"
+			status_code  = 404
+		}
+	}
+}
+
+resource "aws_security_group" "http-alb" {
+	name = "http-alb"
+  
+	ingress {
+		from_port   = 80
+		to_port     = 80
+		protocol    = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+		from_port   = 0
+		to_port     = 0
+		protocol    = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
+resource "aws_lb_target_group" "web-cluster-asg" {
+	name     = "terraform-asg-example"
+  port     = var.server_port
+	protocol = "HTTP"
+	vpc_id   = data.aws_vpc.default.id
+
+	health_check {
+		path                = "/"
+		protocol            = "HTTP"
+		matcher             = "200"
+		interval            = 15
+		timeout             = 3
+		healthy_threshold   = 2
+		unhealthy_threshold = 2
+	}
+}
+
+resource "aws_lb_listener_rule" "web-cluster-asg" {
+	listener_arn = aws_lb_listener.http.arn
+	priority     = 100
+
+	condition {
+		field  = "path-pattern"
+    values = ["*"]
+	}
+
+	action {
+		type = "forward"
+		target_group_arn = aws_lb_target_group.web-cluster-asg.arn
+	}
+}
 
 ##################
 #                #
@@ -115,3 +192,6 @@ resource "aws_autoscaling_group" "webcluster-asg" {
 #                #
 ##################
 
+output "alb_dns_name" {
+	value = aws_lb.darkcityagent-dot-com.dns_name
+}
